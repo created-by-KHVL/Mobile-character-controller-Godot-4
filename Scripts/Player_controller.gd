c4871f6b00185a26addb47264af7d04200881ba3
created-by-KHVL <vlad.rezerv1@gmail.com>
2024-03-28 18:19:13 +0300

    Initial commit

---
+++ Scripts/Player_controller.gd
@@ -0,0 +1,88 @@
+extends CharacterBody3D
+
+@export var touch_input: Node
+@export var camera: Camera3D
+
+@export var speed: float = 4
+
+const JUMP_VELOCITY = 4.5
+
+var start_speed: float
+var start_camera_fov: float
+
+var is_jumping: bool = false
+var is_running: bool = false 
+
+var input_direction: Vector2
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	start_speed = speed
+	start_camera_fov = camera.fov
+
+func _process(delta):
+	if Input.is_action_pressed("run") and abs(input_direction) > Vector2.ZERO:
+		is_running = true
+	else:
+		is_running = false
+		
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		is_jumping = true
+		velocity.y = JUMP_VELOCITY
+	# Move player
+	if is_on_floor():
+		is_jumping = false
+		player_movement()
+	# Player rotation
+	if not is_jumping and not is_running:
+		player_rotation()
+
+	move_and_slide()
+
+func player_movement():
+	var fov_change_speed: int = 2
+	if is_running:
+		var acceleration: int = 4
+		speed = start_speed + acceleration
+		camera.fov = move_toward(camera.fov, start_camera_fov + 15, fov_change_speed)
+	else:
+		speed = start_speed
+		camera.fov = move_toward(camera.fov, start_camera_fov, fov_change_speed)
+	
+	input_direction = Input.get_vector("move_left", "move_right", "move_foward", "move_back")
+	if touch_input != null:
+		if touch_input.is_left_finger:
+			input_direction = touch_input.left_finger_direction
+	var direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		
+func player_rotation():
+	var sensitivity: float
+	# Gamepad input
+	var controller_view_rotation = Input.get_vector("look_up", "look_down", "look_left", "look_right")
+	# Touch input
+	if touch_input != null:
+		if touch_input.is_right_finger:
+			sensitivity = .6
+			controller_view_rotation = touch_input.right_finger_direction
+			camera.rotation_degrees.x -= controller_view_rotation.y * sensitivity 
+			rotation_degrees.y -= controller_view_rotation.x * sensitivity 
+	else:
+		sensitivity = 2
+		camera.rotation_degrees.x -= controller_view_rotation.x * sensitivity 
+		rotation_degrees.y -= controller_view_rotation.y * sensitivity
+	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
---
+++ Scripts/Touch_input_.gd
@@ -0,0 +1,73 @@
+extends Node
+
+var touch_points: Dictionary = {}
+
+var is_left_finger: bool = false
+var is_right_finger: bool = false
+
+var first_left_finger_index: int = -1
+var first_right_finger_index: int = -1
+
+var half_screen_size: Vector2
+var left_finger_start_position: Vector2
+var right_finger_start_position: Vector2
+var left_finger_current_position: Vector2
+var right_finger_current_position: Vector2
+var left_finger_direction: Vector2
+var right_finger_direction: Vector2
+
+func _ready():
+	half_screen_size = Vector2(DisplayServer.screen_get_size().x / 2, 0)
+
+func _process(delta):
+	if is_left_finger:
+		left_finger_action()
+	else:
+		left_finger_direction = Vector2.ZERO
+	
+	if is_right_finger:
+		right_finger_action()
+	else:
+		right_finger_direction = Vector2.ZERO
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		handle_touch(event)
+	elif event is InputEventScreenDrag:
+		handle_drag(event)
+
+func handle_touch(event: InputEventScreenTouch):
+	if event.pressed:
+		if event.index not in touch_points:
+			touch_points[event.index] = event.position
+		
+			if event.position.x < half_screen_size.x and not is_left_finger:
+				is_left_finger = true
+				left_finger_start_position = event.position
+				first_left_finger_index = event.index
+			elif event.position.x >= half_screen_size.x and not is_right_finger:
+				is_right_finger = true
+				right_finger_start_position = event.position
+				first_right_finger_index = event.index
+	else:
+		if event.index == first_left_finger_index:
+			is_left_finger = false
+			left_finger_start_position = Vector2.ZERO
+			first_left_finger_index = -1
+		elif event.index == first_right_finger_index:
+			is_right_finger = false
+			right_finger_start_position = Vector2.ZERO
+			first_right_finger_index = -1
+		touch_points.erase(event.index)
+
+func handle_drag(event: InputEventScreenDrag):
+	if event.index == first_left_finger_index:
+		left_finger_current_position = event.position
+	elif event.index == first_right_finger_index:
+		right_finger_current_position = event.position
+
+func left_finger_action():
+	left_finger_direction = left_finger_start_position.direction_to(left_finger_current_position)
+
+func right_finger_action():
+	right_finger_direction = right_finger_start_position.direction_to(right_finger_current_position)
